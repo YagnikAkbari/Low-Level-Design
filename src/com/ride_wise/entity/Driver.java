612df471fe29ef2482c2b089e96f06e03c342bbd
YagnikAkbari2003 <yagnik.a@medkart.in>
2026-06-20 14:02:09 +0530

    Add Getter, Setter and Construcutors for all existing entities.

--- src/com/ride_wise/entity/Driver.java
+++ src/com/ride_wise/entity/Driver.java
@@ -12,4 +12,88 @@ public class Driver {
   private String currentLocation;
   private boolean available;
   private VehicleType vehicleType;
+
+  public Driver(long id, String name, int age, String dlNumber, String aadharNumber, String vehicleNumber, String currentLocation, boolean available, VehicleType vehicleType) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.dlNumber = dlNumber;
+    this.aadharNumber = aadharNumber;
+    this.vehicleNumber = vehicleNumber;
+    this.currentLocation = currentLocation;
+    this.available = available;
+    this.vehicleType = vehicleType;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getDlNumber() {
+    return dlNumber;
+  }
+
+  public void setDlNumber(String dlNumber) {
+    this.dlNumber = dlNumber;
+  }
+
+  public String getAadharNumber() {
+    return aadharNumber;
+  }
+
+  public void setAadharNumber(String aadharNumber) {
+    this.aadharNumber = aadharNumber;
+  }
+
+  public String getVehicleNumber() {
+    return vehicleNumber;
+  }
+
+  public void setVehicleNumber(String vehicleNumber) {
+    this.vehicleNumber = vehicleNumber;
+  }
+
+  public String getCurrentLocation() {
+    return currentLocation;
+  }
+
+  public void setCurrentLocation(String currentLocation) {
+    this.currentLocation = currentLocation;
+  }
+
+  public boolean isAvailable() {
+    return available;
+  }
+
+  public void setAvailable(boolean available) {
+    this.available = available;
+  }
+
+  public VehicleType getVehicleType() {
+    return vehicleType;
+  }
+
+  public void setVehicleType(VehicleType vehicleType) {
+    this.vehicleType = vehicleType;
+  }
 }
--- src/com/ride_wise/entity/Ride.java
+++ src/com/ride_wise/entity/Ride.java
@@ -8,4 +8,52 @@ public class Ride {
   private Rider rider;
   private double distance;
   private RideStatus rideStatus;
+
+  public Ride(long id, Driver driver, Rider rider, double distance, RideStatus rideStatus) {
+    this.id = id;
+    this.driver = driver;
+    this.rider = rider;
+    this.distance = distance;
+    this.rideStatus = rideStatus;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public Driver getDriver() {
+    return driver;
+  }
+
+  public void setDriver(Driver driver) {
+    this.driver = driver;
+  }
+
+  public Rider getRider() {
+    return rider;
+  }
+
+  public void setRider(Rider rider) {
+    this.rider = rider;
+  }
+
+  public double getDistance() {
+    return distance;
+  }
+
+  public void setDistance(double distance) {
+    this.distance = distance;
+  }
+
+  public RideStatus getRideStatus() {
+    return rideStatus;
+  }
+
+  public void setRideStatus(RideStatus rideStatus) {
+    this.rideStatus = rideStatus;
+  }
 }
